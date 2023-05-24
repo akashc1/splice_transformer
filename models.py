7a77f7890b3cea1de2c4495b2ace4fba9d566238
Akash Chaurasia <akashchaurasia17@yahoo.com>
2023-05-24 22:36:23 +0000

    k, w -> int in mdl init, fix kernel shape

--- models.py
+++ models.py
@@ -12,15 +12,26 @@ CONV_MODEL_CONFIG = {
     },
     400: {
         'kernel_sizes': 11 * np.ones(8, dtype=int),
-        'dilations': np.concatenate((np.ones(4), 4 * np.ones(4))),
+        'dilations': np.concatenate((np.ones(4, dtype=int), 4 * np.ones(4, dtype=int))),
     },
     2000: {
-        'kernel_sizes': np.concatenate((11 * np.ones(8), 21 * np.ones(4))),
-        'dilations': np.concatenate((np.ones(4), 4 * np.ones(4), 10 * np.ones(4))),
+        'kernel_sizes': np.concatenate((11 * np.ones(8, dtype=int), 21 * np.ones(4, dtype=int))),
+        'dilations': np.concatenate(
+            (np.ones(4, dtype=int), 4 * np.ones(4, dtype=int), 10 * np.ones(4, dtype=int)),
+        ),
     },
     10000: {
-        'kernel_sizes': np.concatenate((11 * np.ones(8), 21 * np.ones(4), 41 * np.ones(4))),
-        'dilations': np.concatenate((np.ones(4), 4 * np.ones(4), 10 * np.ones(4), 25 * np.ones(4))),
+        'kernel_sizes': np.concatenate(
+            (11 * np.ones(8, dtype=int), 21 * np.ones(4, dtype=int), 41 * np.ones(4, dtype=int))
+        ),
+        'dilations': np.concatenate(
+            (
+                np.ones(4, dtype=int),
+                4 * np.ones(4, dtype=int),
+                10 * np.ones(4, dtype=int),
+                25 * np.ones(4, dtype=int)
+            )
+        ),
     },
 }
 
@@ -35,10 +46,10 @@ class ResidualUnit(nn.Module):
         self.net = nn.Sequential([
             nn.BatchNorm(),
             nn.relu,
-            nn.Conv(self.dim, self.k, kernel_dilation=self.dilation),
+            nn.Conv(self.dim, (self.k,), kernel_dilation=self.dilation),
             nn.BatchNorm(),
             nn.relu,
-            nn.Conv(self.dim, self.k, kernel_dilation=self.dilation),
+            nn.Conv(self.dim, (self.k,), kernel_dilation=self.dilation),
             nn.relu,
         ])
 
@@ -59,18 +70,18 @@ class DilatedConvSplicePredictor(nn.Module):
 
     @nn.compact
     def __call__(self, x):
-        conv = nn.Conv(self.dim, 1, name='init_conv')(x)
-        skip = nn.Conv(self.dim, 1, name='init_skip')(x)
+        conv = nn.Conv(self.dim, (1,), name='init_conv')(x)
+        skip = nn.Conv(self.dim, (1,), name='init_skip')(x)
 
         for i, (w, d) in enumerate(zip_equal(self.kernel_sizes, self.dilations)):
             conv = ResidualUnit(self.dim, w, d, name=f'residual{i}')(conv)
 
             if (i + 1) % 4 == 0 or i == len(self.kernel_sizes) - 1:
-                dense = nn.Conv(self.dim, 1, name=f'dense{i // 4}')(conv)
+                dense = nn.Conv(self.dim, (1,), name=f'dense{i // 4}')(conv)
                 skip = skip + dense
 
         skip = skip[:, self.context_length // 2:-(self.context_length // 2), ...]
-        return nn.Conv(3, 1, name='cls_final')(skip)
+        return nn.Conv(3, (1,), name='cls_final')(skip)
 
 
 def get_conv_model(context_length: int):
